5974a8aa768b3ed3d512de2e869730952766a941
nasim <nasimniaziann@gmail.com>
2024-02-17 09:37:00 +0330

    Implement data formatting function and export for reuse

--- js/game.js
+++ js/game.js
@@ -1,3 +1,5 @@
+import formatData from "./helper.js";
+
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
 
@@ -9,8 +11,7 @@ let formattedData = null;
 const fetchData = async () => {
   const response = await fetch(URL);
   const json = await response.json();
-  console.log(json);
-  formattedData = json;
+  formattedData = formatData(json.results);
   start();
 };
 
