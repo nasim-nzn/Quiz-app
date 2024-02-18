36de31eb4f29e4083dfa7c4f0f0aee63c2412cf6
nasim <nasimniaziann@gmail.com>
2024-02-18 19:13:16 +0330

    Display high scores

--- js/end.js
+++ js/end.js
@@ -1,5 +1,5 @@
 const score = JSON.parse(localStorage.getItem("score"));
-const highScore = JSON.parse(localStorage.getItem("highScore")) || [];
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 const scoreEle = document.querySelector("p");
 const button = document.querySelector("button");
@@ -12,11 +12,11 @@ const saveHanler = () => {
     alert("Invalid username or score!");
   } else {
     const finalScore = { name: input.value, score };
-    highScore.push(finalScore);
-    highScore.sort((a, b) => b.score - a.score);
-    highScore.splice(10);
+    highScores.push(finalScore);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(10);
 
-    localStorage.setItem("highScore", JSON.stringify(highScore));
+    localStorage.setItem("highScores", JSON.stringify(highScores));
     localStorage.removeItem(score);
     window.location.assign("/");
   }
